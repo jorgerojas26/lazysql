31e975065f5bf0ccffb5209197891a097755bf2f
Jorge Rojas <jorgeluisrojasb@gmail.com>
2023-10-14 16:08:57 -0400

    Removes mysql logs

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"io"
 	"lazysql/app"
 	"lazysql/pages"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	mysql.SetLogger(log.New(io.Discard, "", 0))
 	if err := app.App.SetRoot(pages.AllPages, true).Run(); err != nil {
 		panic(err)
 	}
