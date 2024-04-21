cd0e99760fd172c0dd09d55be028026979a29b33
LinPr <314573849@qq.com>
2024-04-21 23:00:16 +0800

    feat: add '?' key in order to show help informations

--- components/HelpModal.go
+++ components/HelpModal.go
@@ -7,11 +7,15 @@ import (
 	"github.com/rivo/tview"
 )
 
+type KD struct {
+	Key  string
+	Desc string
+}
 type KeyBindings struct {
-	Global    map[string]string
-	Table     map[string]string
-	Tree      map[string]string
-	SQLEditor map[string]string
+	Global    []KD
+	Table     []KD
+	Tree      []KD
+	SqlEditor []KD
 }
 
 func (kb KeyBindings) String() string {
@@ -19,26 +23,26 @@ func (kb KeyBindings) String() string {
 
 	// Global
 	result += "---Global---\n"
-	for key, value := range kb.Global {
-		result += fmt.Sprintf("%s: %s\n", key, value)
+	for _, kd := range kb.Global {
+		result += fmt.Sprintf("%s: %s\n", kd.Key, kd.Desc)
 	}
 
 	// Table
 	result += "\n---Table---\n"
-	for key, value := range kb.Table {
-		result += fmt.Sprintf("%s: %s\n", key, value)
+	for _, kd := range kb.Table {
+		result += fmt.Sprintf("%s: %s\n", kd.Key, kd.Desc)
 	}
 
 	// Tree
 	result += "\n---Tree---\n"
-	for key, value := range kb.Tree {
-		result += fmt.Sprintf("%s: %s\n", key, value)
+	for _, kd := range kb.Tree {
+		result += fmt.Sprintf("%s: %s\n", kd.Key, kd.Desc)
 	}
 
 	// SQLEditor
 	result += "\n---SQLEditor---\n"
-	for key, value := range kb.SQLEditor {
-		result += fmt.Sprintf("%s: %s\n", key, value)
+	for _, kd := range kb.SqlEditor {
+		result += fmt.Sprintf("%s: %s\n", kd.Key, kd.Desc)
 	}
 	return result
 }
@@ -49,31 +53,31 @@ type HelpModal struct {
 
 func NewHelpModal() *HelpModal {
 	keybindings := KeyBindings{
-		Global: map[string]string{
-			"?":         "Show help informations",
-			"CTRL+e":    "Open SQL editor",
-			"BACKSPACE": "Return to connection selection",
+		Global: []KD{
+			{"?", "Show help informations"},
+			{"CTRL+e", "Open SQL editor"},
+			{"BACKSPACE", "Return to connection selection"},
 		},
-		Table: map[string]string{
-			"c":      "Edit table cell",
-			"d":      "Delete row",
-			"o":      "Add row",
-			"/":      "Focus the filter input or SQL editor",
-			"CTRL+s": "Commit changes",
-			">":      "Next page",
-			"<":      "Previous page",
-			"K":      "Sort ASC",
-			"J":      "Sort DESC",
-			"H":      "Focus tree panel",
-			"[":      "Focus previous tab",
-			"]":      "Focus next tab",
-			"X":      "Close current tab",
+		Table: []KD{
+			{"c", "Edit table cell"},
+			{"d", "Delete row"},
+			{"o", "Add row"},
+			{"/", "Focus the filter input or SQL editor"},
+			{"CTRL+s", "Commit changes"},
+			{">", "Next page"},
+			{"<", "Previous page"},
+			{"K", "Sort ASC"},
+			{"J", "Sort DESC"},
+			{"H", "Focus tree panel"},
+			{"[", "Focus previous tab"},
+			{"]", "Focus next tab"},
+			{"X", "Close current tab"},
 		},
-		Tree: map[string]string{
-			"L": "Focus table pane",
+		Tree: []KD{
+			{"L", "Focus table pane"},
 		},
-		SQLEditor: map[string]string{
-			"CTRL+R": "Run the SQL statement",
+		SqlEditor: []KD{
+			{"CTRL+R", "Run the SQL statement"},
 		},
 	}
 	modal := tview.NewModal().
