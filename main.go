bcb8edf338df85e12c65612080316ecdf8950696
Jorge Rojas <jorgeluisrojasb@gmail.com>
2024-07-09 23:37:27 -0400

    add base structure to allow logging to a file

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 
 	"github.com/jorgerojas26/lazysql/app"
 	"github.com/jorgerojas26/lazysql/components"
+	"github.com/jorgerojas26/lazysql/helpers/logger"
 
 	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	rawLogLvl := flag.String("loglvl", "info", "Log level")
+	logFile := flag.String("logfile", "", "Log file")
+	flag.Parse()
+
+	logLvl, err := logger.ParseLogLevel(*rawLogLvl)
+	if err != nil {
+		panic(err)
+	}
+	logger.SetLevel(logLvl)
+	logger.SetFile(*logFile)
+	logger.Info("Starting LazySQL...", nil)
+
 	mysql.SetLogger(log.New(io.Discard, "", 0))
 
 	if err := app.App.
