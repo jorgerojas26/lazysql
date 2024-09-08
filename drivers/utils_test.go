8c50b79b6dca2b902ef782564ce0e777db706752
Tomáš Procházka <tomas.prochazka5d@gmail.com>
2024-09-08 23:41:14 +0200

    add tests for queriesInTransaction

---
+++ drivers/utils_test.go
@@ -0,0 +1,116 @@
+package drivers
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	gomock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/jorgerojas26/lazysql/models"
+)
+
+func Test_queriesInTransaction(t *testing.T) {
+	tests := []struct {
+		name                string
+		queries             []models.Query
+		setMockExpectations func(mock gomock.Sqlmock)
+		assertErr           func(t *testing.T, err error)
+	}{
+		{
+			name: "successful transaction",
+			queries: []models.Query{
+				{Query: "SELECT * FROM table"},
+			},
+			setMockExpectations: func(mock gomock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnResult(gomock.NewResult(0, 0))
+				mock.ExpectCommit()
+			},
+		},
+		{
+			name: "unsuccessful commit",
+			queries: []models.Query{
+				{Query: "SELECT * FROM table"},
+			},
+			setMockExpectations: func(mock gomock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnResult(gomock.NewResult(0, 0))
+				mock.ExpectCommit().WillReturnError(errors.New("commit error"))
+			},
+			assertErr: func(t *testing.T, err error) {
+				if !strings.Contains(err.Error(), "commit error") {
+					t.Errorf("expected error to contain 'commit error', got %v", err)
+				}
+			},
+		},
+		{
+			name: "failed query",
+			queries: []models.Query{
+				{Query: "SELECT * FROM table"},
+			},
+			setMockExpectations: func(mock gomock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnError(errors.New("query error"))
+				mock.ExpectRollback()
+			},
+			assertErr: func(t *testing.T, err error) {
+				if !strings.Contains(err.Error(), "query error") {
+					t.Errorf("expected error to contain 'commit error', got %v", err)
+				}
+			},
+		},
+		{
+			name: "failed 2nd query of three",
+			queries: []models.Query{
+				{Query: "SELECT * FROM table"},
+				{Query: "SELECT * FROM table"},
+				{Query: "SELECT * FROM table"},
+			},
+			setMockExpectations: func(mock gomock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnResult(gomock.NewResult(0, 0))
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnError(errors.New("query error"))
+				mock.ExpectRollback()
+			},
+			assertErr: func(t *testing.T, err error) {
+				if !strings.Contains(err.Error(), "query error") {
+					t.Errorf("expected error to contain 'commit error', got %v", err)
+				}
+			},
+		},
+		{
+			name: "failed query and rollback",
+			queries: []models.Query{
+				{Query: "SELECT * FROM table"},
+			},
+			setMockExpectations: func(mock gomock.Sqlmock) {
+				mock.ExpectBegin()
+				mock.ExpectExec("SELECT \\* FROM table").WillReturnError(errors.New("query error"))
+				mock.ExpectRollback().WillReturnError(errors.New("rollback error"))
+			},
+			assertErr: func(t *testing.T, err error) {
+				errMsg := err.Error()
+				if !strings.Contains(errMsg, "query error") {
+					t.Errorf("expected error to contain 'commit error', got %v", err)
+				}
+				if !strings.Contains(errMsg, "rollback error") {
+					t.Errorf("expected error to contain 'rollback error', got %v", err)
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, mock, err := gomock.New()
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer db.Close()
+			tt.setMockExpectations(mock)
+			queryErr := queriesInTransaction(db, tt.queries)
+			if tt.assertErr != nil {
+				tt.assertErr(t, queryErr)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/jorgerojas26/lazysql
 go 1.20
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/atotto/clipboard v0.1.4
 	github.com/gdamore/tcell/v2 v2.7.0
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/uuid v1.6.0
+	github.com/lib/pq v1.10.9
 	github.com/pelletier/go-toml/v2 v2.1.1
 	github.com/rivo/tview v0.0.0-20240101144852-b3bd1aa5e9f2
 	github.com/xo/dburl v0.20.2
@@ -15,25 +17,21 @@ require (
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
 	modernc.org/libc v1.55.3 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
 	modernc.org/memory v1.8.0 // indirect
 	modernc.org/strutil v1.2.0 // indirect
 	modernc.org/token v1.1.0 // indirect
-)
-
-require (
-	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/lib/pq v1.10.9
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 )
